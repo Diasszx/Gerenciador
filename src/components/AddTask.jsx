7a7aa50ecf7f8acfbc1ed7672f7684e0b6b75355
Adam_Diass <adamrpdiass@hotmail.com>
2024-09-04 16:03:49 -0300

    feat: add task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,32 @@
-function AddTask() {
-  <h1>Add task</h1>;
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("1");
+  const [description, setDescription] = useState("2");
+  return (
+    <h1 className="space-y-4 p-6 bg-slate-200 rounded-md shadow-2xl flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite o título da tarefa"
+        className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        onClick={() => onAddTaskSubmit(title, description)}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+      >
+        Adicionar
+      </button>
+    </h1>
+  );
 }
 
 export default AddTask;
