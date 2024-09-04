cc68d2d45eb2fbb938f9cf4163e904ff8e458194
Adam_Diass <adamrpdiass@hotmail.com>
2024-09-04 16:29:17 -0300

    feat: uuid

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -20,7 +20,15 @@ function AddTask({ onAddTaskSubmit }) {
         onChange={(event) => setDescription(event.target.value)}
       />
       <button
-        onClick={() => onAddTaskSubmit(title, description)}
+        onClick={() => {
+          // VERIFICAR IS NOT EMPTY
+          if (!title.trim() || !description.trim()) {
+            return alert("Preencha o título e a descrição da tarefa");
+          }
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
+        }}
         className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
       >
         Adicionar
