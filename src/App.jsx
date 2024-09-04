ea03ab52eb6692b48dc48d42779092d9bee5c7a8
Adam_Diass <adamrpdiass@hotmail.com>
2024-09-04 09:18:17 -0300

    feat: Tasks layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programacao",
+      description:
+        "Estudar programação para se tornar um desenovlver full stack",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar ingles",
+      description: "Estudar inglês para se tornar bilingue",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Ler um livro",
+      description: "Ler clean code",
+      isCompleted: false,
+    },
+  ]);
+
   return (
-    <div>
-      <h1 className="text-red-500 text-3xl">Gerenciador de Tarefas</h1>
-      <Tasks />
-      <AddTask />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <Tasks tasks={tasks} />
+        <AddTask />
+      </div>
     </div>
   );
 }
